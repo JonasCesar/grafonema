c041ce9152f87a82edff2961955a15febafab0d2
iran <iran@iran>
2017-09-20 20:33:04 -0300

    Merge origin/master

--- src/model/Jogador.java
+++ src/model/Jogador.java
@@ -20,10 +20,10 @@ public class Jogador {
      * Construtor da classe Jogador
      */
     public Jogador() {
-        this.faseAtual = 1;
+        this.faseAtual = 7;
         this.qntPulos = 0;
         this.pontuacaoTotal = 0;
-        this.acertosTotal = 0;
+        this.acertosTotal = 10;
         this.qntErros = 0;
         this.estaComBonus = false;
         this.acertosPorFase = new ArrayList();
